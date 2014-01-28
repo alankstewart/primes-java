4afd3fc19a3da27a57b2d85cb6a42c8792d64648
Alan Stewart <alankstewart@gmail.com>
2014-01-28 18:16:44 +1100

    Fix logic for thread get

--- src/main/java/alankstewart/primes/Primes.java
+++ src/main/java/alankstewart/primes/Primes.java
@@ -17,52 +17,45 @@ public class Primes {
     private static final boolean DEBUG = Boolean.getBoolean("debug");
 
     public static void main(final String[] args) {
-        int upperBound = 100;
-        if (args != null && args.length > 0) {
-            upperBound = parseInt(args[0]);
-        }
-
         final Console console = System.console();
         if (console == null) {
             err.format("No console\n");
             exit(1);
         }
 
-        out.format("Enter a number between 2 and %d: ", upperBound);
-        int number = parseInt(console.readLine());
-        if (number < 2 || number > upperBound) {
-            err.format("Number must be between 2 and %d\n", upperBound);
-            exit(1);
-        }
-
+        final int upperBound = args != null && args.length > 0 ? parseInt(args[0]) : 100;
         final Primes primes = new Primes();
-        primes.calculate(upperBound, number);
-    }
-
-    public void calculate(final int upperBound, int number) {
         final ExecutorService executorService = Executors.newSingleThreadExecutor();
         final Future<boolean[]> future = executorService.submit(new Callable<boolean[]>() {
             @Override
             public boolean[] call() throws Exception {
-                return eratosthenesSieve(upperBound);
+                return primes.eratosthenesSieve(upperBound);
             }
         });
 
         try {
+            out.format("Enter a number between 2 and %d: ", upperBound);
             final boolean[] composite = future.get();
-            if (DEBUG) {
-                for (int i = 2; i <= upperBound; i++) {
-                    if (!composite[i]) {
-                        out.format("%d ", i);
-                    }
-                }
-                out.format("\n");
+            int number = parseInt(console.readLine());
+            if (number < 2 || number > upperBound) {
+                err.format("Number must be between 2 and %d\n", upperBound);
+                exit(1);
             }
 
             if (composite[number]) {
                 out.format("%d is not prime\n", number);
             } else {
                 out.format("%d is prime\n", number);
+            }
+
+            if (DEBUG) {
+                out.format("\n");
+                for (int i = 2; i <= upperBound; i++) {
+                    if (!composite[i]) {
+                        out.format("%d ", i);
+                    }
+                }
+                out.format("\n");
             }
         } catch (InterruptedException | ExecutionException e) {
             throw new IllegalStateException(e);
